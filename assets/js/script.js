af10b9cd7a46b7c68b7f40b22bba15848306a47a
Macheya Steele <mcheysteele@gmail.com>
2020-12-14 20:50:03 -0700

    current weather API, event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,49 @@
+var apiID = "ba2728cbdcc99be3d1d810cfd5281dc0";
+var incorrectWarningEl = document.querySelector("#incorrect-warning")
+var city = "Salt Lake City";
+
+var searchCityButtonEl = document.querySelector("#search-btn");
 
 
 
+//City Search
+var buttonClickHandler = function (event) {
+    console.log("itworked");
+    
+    
+}
+
+//Current Weather function
+var getCityWeather = function() {
+    console.log(apiID);
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=ba2728cbdcc99be3d1d810cfd5281dc0";
+    //request
+fetch (apiUrl).then(function(response) {
+    //request was successful
+    if (response.ok) {
+        return (response.json())
+       // displayCityWeather(); 
+    //request was unsuccessful
+    } else {
+            displayError();
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to complete search");
+        
+    });
+    };
+
+
+
+    var displayError = function () {
+        incorrectWarningEl.textContent = "Please enter a valid city name";
+     }
+
+     //var displayCityWeather = function () {
+
+     //}
+
+
+searchCityButtonEl.addEventListener("click", buttonClickHandler);
+getCityWeather();
