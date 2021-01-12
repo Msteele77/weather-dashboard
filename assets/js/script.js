f5a4dd884b37c543801b1dd47280fd39ac3fe095
Macheya Steele <mcheysteele@gmail.com>
2021-01-12 12:02:56 -0700

    weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,79 @@
-var apiID = "ba2728cbdcc99be3d1d810cfd5281dc0";
-var incorrectWarningEl = document.querySelector("#incorrect-warning")
-var city = "Salt Lake City";
 
-var searchCityButtonEl = document.querySelector("#search-btn");
-
+//var incorrectWarningEl = document.querySelector("#incorrect-warning")
+//var city = data.name
+//var city = document.querySelector("#city-search").value
 
 
-//City Search
-var buttonClickHandler = function (event) {
-    console.log("itworked");
+//var lon = data.coord.lon
+//var lat = data.coord.lat 
+
+
+//when search button is clicked
+var getCurrentConditions = function (city) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ba2728cbdcc99be3d1d810cfd5281dc0')
+    .then(function(response) {
+        return response.json() }) //Converts to json
+    .then(function(data) {
+        console.log(data);
+    })
+    .catch(function() {//catches errors
+    });
     
-    
+   
 }
 
+
+//var searchCityButtonEl = document.querySelector("#search-btn");
+//searchCityButtonEl.addEventListener("click", getCurrentConditions);
+
+
+getCurrentConditions();
+
+
+
+
+
 //Current Weather function
-var getCityWeather = function() {
-    console.log(apiID);
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=ba2728cbdcc99be3d1d810cfd5281dc0";
-    //request
-fetch (apiUrl).then(function(response) {
+/*var getCurrentConditions = function(units, city) {
+    //Request
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=ba2728cbdcc99be3d1d810cfd5281dc0")
+    .then(function(response) {
     //request was successful
     if (response.ok) {
         return (response.json())
-       // displayCityWeather(); 
-    //request was unsuccessful
     } else {
-            displayError();
-        }
+    //request unsuccessful
+        throw new error("Not succesful.")
+    }
+
     })
+    //returning data
+    .then(function(data) {
+    //varialbes for request data
+     
+    fetch("https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=ba2728cbdcc99be3d1d810cfd5281dc0")
+        .then(function(response) {
+            if (response.ok) {
+                console.log("SUCCESS")
+                //return (response.json())
+            } else {
+                console.log("NOT SUCCESSFUL")
+            }
+            response.json()
+            }).then(function(data) {
+                console.log(data);
+            })
+        })
     .catch(function(error) {
-        alert("Unable to complete search");
-        
-    });
-    };
+        console.log("ERROR");
+      });
+      }
 
+*/
 
-
-    var displayError = function () {
-        incorrectWarningEl.textContent = "Please enter a valid city name";
-     }
-
-     //var displayCityWeather = function () {
-
-     //}
-
-
-searchCityButtonEl.addEventListener("click", buttonClickHandler);
-getCityWeather();
+    
+    
+    
+    
+   
+  
